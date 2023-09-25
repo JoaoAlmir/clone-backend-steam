3c238f974a71c234af4d4c31f563e0b14d1a600c
JoaoAlmir <junivion@gmail.com>
2023-09-24 22:40:22 -0300

    first commit

---
+++ SteamBackend/src/main/java/br/com/quixada/ufc/SteamBackend/SteamBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.quixada.ufc.SteamBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamBackendApplication.class, args);
+	}
+
+}
---
+++ SteamBackend/src/test/java/br/com/quixada/ufc/SteamBackend/SteamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.quixada.ufc.SteamBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
