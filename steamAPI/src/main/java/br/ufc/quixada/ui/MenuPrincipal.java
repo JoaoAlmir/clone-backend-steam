5e5b18921af2ada353b7e7879411a771cfd63480
JoaoAlmir <joaoalmir@alu.ufc.br>
2023-12-02 22:18:03 -0300

    add EnableMongoRepositories

--- steamAPI/src/main/java/br/ufc/quixada/ui/MenuPrincipal.java
+++ steamAPI/src/main/java/br/ufc/quixada/ui/MenuPrincipal.java
@@ -7,13 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication(scanBasePackages = "br.ufc.quixada")
 @EntityScan("br.ufc.quixada.entity")
 @EnableJpaRepositories("br.ufc.quixada.dao.jpa")
+// @EnableMongoRepositories("br.ufc.quixada.dao.mongo")
 @Slf4j
 public class MenuPrincipal implements CommandLineRunner {
 
